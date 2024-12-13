8ba24ea077b6a61d214668ce36a5d7bf76d8a480
ptk tjamtg <swdfgpiki8810@gmail.com>
2024-12-14 04:33:46 +0900

    保存した掲示板データの一覧表示とタイムゾーンの設定、コントローラとルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   get "boards", to: "boards#index"
   get "boards/new", to: "boards#new"
-  post "/boards", to: "board#create"
+  post "boards", to: "boards#create"
 end
