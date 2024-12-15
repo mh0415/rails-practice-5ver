679b1687743f16b3a6d6d9531041d1d91cb6fdc0
ptk tjamtg <swdfgpiki8810@gmail.com>
2024-12-16 00:06:39 +0900

    アソシエーションのdependentオプション

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,6 @@
 class Board < ApplicationRecord
-    has_many :comments
-    has_many :board_tag_relations
+    has_many :comments, dependet: :delete_all
+    has_many :board_tag_relations, dependet: :delete_all
     has_many :tags, through: :board_tag_relations
     validates :name, presence: true, length: {maximum: 10}
     validates :title, presence: true, length: {maximum: 30}
