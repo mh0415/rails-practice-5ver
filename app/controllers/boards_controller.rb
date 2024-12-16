dca1c0a7bb7908ad6ec926122be2a7b172ef0944
ptk tjamtg <swdfgpiki8810@gmail.com>
2024-12-16 21:32:53 +0900

    タグを使用した掲示板検索機能の追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,7 @@ class BoardsController < ApplicationController
     before_action :set_target_board, only: %i[show edit update destroy]
   
     def index
-      @boards = Board.all
+      @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
     end
   
     def new
