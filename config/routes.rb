83001df9554ef59feaffa0fac5f859dce0d58464
ptk tjamtg <swdfgpiki8810@gmail.com>
2024-12-17 21:30:44 +0900

    ユーザー認証機能で使用するルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'new/create'
-  get 'new/me'
-  get 'home/index'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get "mypage", to: "users#me"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
   resources :boards
   resources :comments, only: %i[create destroy]
+  resources :users, only: %i[new create]
 end
